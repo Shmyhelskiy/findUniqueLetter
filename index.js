9fba675d44694bca17427f60abce8256bbd78aaa
Alex Shmyhelskyi <shmyhelskyi.o.v@gmail.com>
2023-06-28 10:27:37 +0300

    completed task

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const findUniqueLetter = (text) => {
+// Приймаємо текст та розбиваємо його на масив слів 
   const words = text.split(" ");
+// Отримуємо обєкт з ключем буквою та значенням кількості повторення букви
   const lettersInWords = words.map((item) => countLetters(item));
+// Отримуємо масив унікальник букв
   const result = lettersInWords.map((item) => findFirstUniqueLetter(item));
+// Масив букв надсилаємо у функцію countLetters для обрахунку кількості повторення букв і потім отримуємо першу унікальну букву з функції findFirstUniqueLetter
   return findFirstUniqueLetter(countLetters(result));
 };
 
 const countLetters = (word) => {
+// Створюємо порожній обєкт 
   const lettersObj = {};
   for (let i = 0; i < word.length; i++) {
+// Цикл перевіряє кожну букву у слові, при перевірці перший раз створює ключ = букву та значення = 1, якщо ж буква повторюється значення збільшується на 1  
     const char = word[i];
     lettersObj[char] ? lettersObj[char]++ : (lettersObj[char] = 1);
   }
@@ -15,6 +21,7 @@ const countLetters = (word) => {
 };
 
 const findFirstUniqueLetter = (word) => {
+// Повернення першої унікальної букви зі слова 
   for (let letter in word) {
     if (word[letter] === 1) {
       return letter;
